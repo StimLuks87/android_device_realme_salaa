019a8091bb35f56ee2310883c9e012d7512c0059
Stim Luks <san10031987san@gmail.com>
2024-06-25 00:51:07 +0500

    fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,4 +40,22 @@ if [ ! -d "packages/apps/GoogleCameraGo" ]; then
 unzip packages/apps/GoogleCameraGo/GoogleCameraGo.zip -d packages/apps/GoogleCameraGo && rm packages/apps/GoogleCameraGo/GoogleCameraGo.zip
 fi
 
+echo 'Cloning vendor [6/7]'
+# Vendor
+rm -rf vendor/realme/salaa
+if [ ! -d "vendor/realme/salaa" ]; then
+    mkdir -p vendor/realme/salaa
+    git clone https://github.com/StimLuks87/vendor_device_realme_salaa.git -b udc vendor/realme/salaa
+unzip vendor/realme/salaa/radio/md1img.zip -d vendor/realme/salaa/radio && rm vendor/realme/salaa/radio/md1img.zip
+unzip vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip -d vendor/realme/salaa/proprietary/odm/lib64 && rm vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip
+fi
+
+echo 'Cloning kernel [7/7]'
+# Kernel
+rm -rf kernel/realme/mt6785
+if [ ! -d "kernel/realme/mt6785" ]; then
+    mkdir -p kernel/realme/mt6785
+    git clone https://github.com/StimLuks87/psionic_kernel_realme_mt6785.git -b android-14 kernel/realme/mt6785
+fi
+
 echo 'Completed, Now proceeding to lunch'
