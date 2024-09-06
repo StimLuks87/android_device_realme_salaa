f33d1e8284385007ee5b8a9f52cdbece22450b58
Stim Luks <san10031987san@gmail.com>
2024-09-06 14:52:15 +0500

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+#
+#  Copyright (C) 2024 The LineageOS Project
+#  SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_salaa.mk
---
+++ vendorsetup.sh
@@ -0,0 +1,40 @@
+# Cloning required repositories for making rum:
+
+# Kernel Source:
+git clone https://github.com/StimLuks87/psionic_kernel_realme_mt6785  kernel/realme/mt6785
+
+# Vendor Source:
+git clone https://github.com/StimLuks87/vendor_device_realme_salaa vendor/realme/salaa
+unzip vendor/realme/salaa/radio/md1img.zip -d vendor/realme/salaa/radio && rm vendor/realme/salaa/radio/md1img.zip
+unzip vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip -d vendor/realme/salaa/proprietary/odm/lib64 && rm vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip
+
+# Hardware of Mediatek and Oplus:
+git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
+git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
+
+# Mediatek SEPolicy VNDR:
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+# bcr
+git clone https://github.com/StimLuks87/vendor_bcr vendor/bcr
+
+# Lineage SEPolicy:
+#git clone https://github.com/LineageOS/android_device_lineage_sepolicy device/lineage/sepolicy
+
+# ViPER4AndroidFX:
+git clone https://github.com/StimLuks87/VIPER_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+# VancedManager
+git clone https://github.com/StimLuks87/VancedManager packages/apps/VancedManager
+
+# CameraGo
+unzip device/realme/salaa/CameraGo/CameraGo.zip -d device/realme/salaa/CameraGo && rm device/realme/salaa/CameraGo/CameraGo.zip
+
+# Make the build faster using ccache
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+ccache -o compression=true
+
+# Disable and stop systemd-oomd service.
+systemctl disable --now systemd-oomd
