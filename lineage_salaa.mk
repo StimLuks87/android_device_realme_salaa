24a22497a66a41d13826c559468ee2b81027dd09
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2026-02-20 23:42:21 +0500

    Clean import of bq2 from local backup

---
+++ lineage_salaa.mk
@@ -0,0 +1,43 @@
+#
+# SPDX-FileCopyrightText: The Android Open Source Project
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit some common Evo-X OS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/realme/salaa/device.mk)
+
+# Evo-X flags
+BUILD_BCR := true
+EVO_BUILD_TYPE := Unofficial
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_ACCORD := true
+
+# Gapps
+WITH_GMS := true
+TARGET_USES_MINI_GAPPS := true
+
+# Boot animation
+TARGET_INCLUDE_BOOT_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device Information
+PRODUCT_NAME := lineage_salaa
+PRODUCT_DEVICE := salaa
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX2151/RMX2155/RMX2156
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+BuildDesc="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1711679158901 release-keys" \
+BuildFingerprint=realme/RMX2156/RMX2156L1:12/SP1A.210812.016/Q.11e8c10-4e353:user/release-keys
